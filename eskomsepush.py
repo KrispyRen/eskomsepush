a5cfa6b70d7915eab2c66312a8c77ffcf76ea763
Chris <chris.puren@xneelo.com>
2022-12-29 01:27:48 +0200

    removed typ hinting

--- eskomsepush.py
+++ eskomsepush.py
@@ -14,7 +14,7 @@ class EskomSePush:
         self.base_url = config('BASE_URL')
 
     #used to make the request
-    def make_request(self, endpoint: str,  payload : dict = None ) -> None:
+    def make_request(self, endpoint: str,  payload : dict = None ):
         query_url = self.base_url + endpoint
         try:
             response = requests.get(url=query_url, headers=self.headers, params=payload)
@@ -37,35 +37,35 @@ class EskomSePush:
             print(f'Request failed: {e}')
 
     #search areas - input is str
-    def search_areas(self, area_search : str) -> dict:
+    def search_areas(self, area_search : str):
         payload = {"text": area_search}
         return self.make_request("/areas_search", payload=payload)
     
     #get the test area information based on the area_id. test can be current or future
-    def get_area_information_test(self) -> dict:
+    def get_area_information_test(self):
         payload = {"id": self.area_id, "test" : "current"}
         return self.make_request("/area", payload=payload)
     
     #get the test area information based on the area_id retrieved from search_areas()
-    def get_area_information(self) -> dict:
+    def get_area_information(self):
         payload = {"id": self.area_id}
         return self.make_request("/area", payload=payload)
 
     #check how many credits you have and limit
-    def get_allowance(self) -> dict:
+    def get_allowance(self):
         return self.make_request("/api_allowance")
 
     #get the national schedule
-    def get_status(self) -> dict:
+    def get_status(self):
         return self.make_request("/status")
     
     #get areas nearby based on latitude and longitude
-    def get_areas_nearby(self, lat, lon) -> dict:
+    def get_areas_nearby(self, lat, lon):
         payload = {"lat": lat, "lon": lon}
         return self.make_request("/areas_nearby", payload=payload)
 
     #get topics nearby created by users
-    def get_topics_nearby(self, lat, lon) -> dict:
+    def get_topics_nearby(self, lat, lon):
         payload = {"lat": lat, "lon": lon}
         return self.make_request("/topics_nearby", payload=payload)
 
